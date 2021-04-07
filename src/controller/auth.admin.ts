149ac99661971250f0d8bbada28f0acb7545c0c5
Anderson <chltjdrhd777@naver.com>
2021-04-07 11:39:35 +0900

    Heroku

--- src/controller/auth.admin.ts
+++ src/controller/auth.admin.ts
@@ -39,34 +39,26 @@ const register = (req: CustomAdminRequest, res: Response) => {
 };
 
 const login = (req: CustomAdminRequest, res: Response) => {
-  Admin.findOne({ email: req.body.email }, null, null, async (err, targetAdmin) => {
+  Admin.findOne({ email: req.body.email }, null, null, (err, targetAdmin) => {
     //conditions/////
     if (err) return res.status(400).json({ success: false, message: "cannot find admin" });
 
     if (!targetAdmin) return res.status(400).json({ success: false, message: "no admin" });
 
-    try {
-      await bcrypt.compare(req.body.password, targetAdmin.password).then((isEqual) => {
-        if (isEqual) return res.status(200).json({ isEqual, targetAdmin });
-        return res.status(400).json({ message: "err" });
-      });
+    targetAdmin.authentification(req.body.password).then((isEqual) => {
+      if (!isEqual) return res.status(400).json({ success: false, message: "wrong password" });
+      return res.status(200).json({ isEqual });
 
-      /*  await targetAdmin.authentification(req.body.password).then((isEqual) => {
-        if (!isEqual) return res.status(400).json({ success: false, message: "wrong password" });
-
-        const token = jwt.sign({ _id: targetAdmin._id, role: targetAdmin.role }, process.env.JWT_SECRET, { expiresIn: "3d" });
-        targetAdmin.token = token;
-        targetAdmin.save();
-        let expireDate = new Date(Date.now() + 60 * 60 * 1000 * 24 * 3);
-        res
-          .cookie("authorized_admin", token, { expires: expireDate })
-          .status(200)
-          .json({ success: true, message: "login complete and token updated", targetAdmin });
-      }); */
-    } catch (err) {
-      return res.status(400).json({ err });
-    }
-    //after/////
+      /* 
+      const token = jwt.sign({ _id: targetAdmin._id, role: targetAdmin.role }, process.env.JWT_SECRET, { expiresIn: "3d" });
+      targetAdmin.token = token;
+      targetAdmin.save();
+      let expireDate = new Date(Date.now() + 60 * 60 * 1000 * 24 * 3);
+      res
+        .cookie("authorized_admin", token, { expires: expireDate })
+        .status(200)
+        .json({ success: true, message: "login complete and token updated", targetAdmin }); */
+    });
   });
 };
 
