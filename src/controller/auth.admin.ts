cebafa04ddcfb8c8a1313ee13bd4ad8fc675ab20
Anderson <chltjdrhd777@naver.com>
2021-04-07 10:02:32 +0900

    doen

--- src/controller/auth.admin.ts
+++ src/controller/auth.admin.ts
@@ -38,7 +38,9 @@ const register = (req: CustomAdminRequest, res: Response) => {
 };
 
 const login = (req: CustomAdminRequest, res: Response) => {
-  Admin.findOne({ email: req.body.email }, null, null, (err, targetAdmin) => {
+  res.status(200).json({ message: "work" });
+  console.log("work");
+  /*   Admin.findOne({ email: req.body.email }, null, null, (err, targetAdmin) => {
     //conditions/////
     if (err) return res.status(400).json({ success: false, message: "cannot find admin" });
 
@@ -57,7 +59,7 @@ const login = (req: CustomAdminRequest, res: Response) => {
         .status(200)
         .json({ success: true, message: "login complete and token updated", targetAdmin });
     });
-  });
+  }); */
 };
 
 const adminLogout = (req: CustomAdminRequest, res: Response) => {
